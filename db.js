47ff553fe063c040b9a166b6f5073ee329befbc2
Callies Dylan <calliesdylan8@gmail.com>
2025-04-01 16:53:59 +0200

    Initial commit

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  '', // Database name
-  '', // User
-  '', // Password
+  process.env.DB_NAME, // Database name
+  process.env.DB_USER, // User
+  process.env.DB_PASSWORD, // Password
   {
-    host: '', // Host
+    host: process.env.DB_HOST, // Host
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
